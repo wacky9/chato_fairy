ab787af01da70e1001836aa239bf7aea378765fc
whbs2002 <whbs2002@gmail.com>
2025-04-03 23:56:12 -0400

    Better scraping

--- Scraping/test_scrape.py
+++ Scraping/test_scrape.py
@@ -17,7 +17,12 @@ def manipulate():
     html_data = file.read()
     soup = bs4.BeautifulSoup(html_data, 'html.parser')
     file = open(DIR_HEAD + "test_file.txt", "w", buffering=1)
-    file.write(soup.find("div",class_="post").get_text())
+    paragraph_list = soup.find("div",class_="post").find_all("p")
+    for paragraph in paragraph_list:
+        # Saves only basic <p> tags
+        if len(paragraph.attrs) == 0:
+            file.write(paragraph.get_text())
+            file.write("\n")
     file.close()
 
 #pull()
