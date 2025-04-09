57b2dea7ce8ab6d92b5abff98039d93c4308e34b
whbs2002 <whbs2002@gmail.com>
2025-04-09 01:49:42 -0400

    Basic RAG features

---
+++ Retrieval/rag_test.py
@@ -0,0 +1,40 @@
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+MSG = "How do tariffs influence the balance between protecting domestic industries and promoting international trade? In what ways can the imposition of tariffs impact global economic relationships and supply chains? What are the long-term effects of sustained tariff policies on national economic growth and consumer welfare?"
+#load file from data
+def load():
+    loader = DirectoryLoader('/app/data', glob='**/*.txt')
+    docs = loader.load()
+    return docs
+
+
+#Split the text
+def split(docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    splits = []
+    for s in text_splitter.split_documents(docs):
+        splits.append(s)
+    return splits
+
+#Store in a vector database
+def store(chunks):
+    vectorstore= Chroma.from_documents(
+        documents=chunks,
+        embedding=OpenAIEmbeddings())
+    return vectorstore.as_retriever(search_kwargs={"k": 1})
+
+#Query the vector database
+def query(message, vectorstore):
+    return vectorstore.similarity_search(message)
+
+docs = load()
+chunks = split(docs)
+vectorstore = store(chunks)
+print(query(MSG))
