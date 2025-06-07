c646d94d9924ca819db22acf126ca4e68eacae6e
whbs2002 <whbs2002@gmail.com>
2025-06-07 02:41:11 -0400

    Wrote evaluation code and switched LLMs

--- eval.py
+++ eval.py
@@ -1,6 +1,9 @@
 # Evaluate the response of the AI model against the consensus of the experts in the IGM survey.
 import json
 import random
+from llm_serve import setup, query_llm
+
+
 # Two arrays of 5 variables. AI array has one 1 and 4 0s. 
 def loss(experts, AI):
     consensus = experts.index(max(experts))
@@ -15,6 +18,7 @@ def load_evaluation():
     return data
 
 def evaluate(n=10):
+    llm = setup()
     # evaluate all the responses in the dataset
     if n <=0:
         pass
@@ -30,10 +34,39 @@ def evaluate(n=10):
             question = experts["question"]
             response = experts["weighted"]
             # Get AI response
-            AI = [0, 0, 0, 0, 0]
-            AI[random.randint(0, 4)] = 1
+            AI = base_response(llm,question)
             total_loss += loss(response, AI)
         print(f"Total loss for {n} evaluations: {total_loss}")
         return total_loss
-    
+
+response_dict = {
+    "Strongly Agree": [1, 0, 0, 0, 0],
+    "Agree": [0, 1, 0, 0, 0],
+    "Uncertain": [0, 0, 1, 0, 0],
+    "Disagree": [0, 0, 0, 1, 0],
+    "Strongly Disagree": [0, 0, 0, 0, 1]
+}
+def base_response(llm,question):
+    file = open('config/eval_prompt.txt', 'r')
+    prompt = file.read()
+    file.close()
+    prompt += '\n' + question + '\nResponse:\n'
+    return get_response(llm, prompt)
+
+def rag_response(llm,question):
+    pass
+
+def get_response(llm,msg):
+    reply = query_llm(msg, llm)
+    #remove the prompt from the reply
+    reply = reply.split("Response:")[-1].strip()
+    #Get first line of the reply
+    reply = reply.split("\n")[0]
+    reply = reply.strip()
+    if reply in response_dict:
+        return response_dict[reply]
+    else:
+        print(f"Unknown response: {reply}")
+        return [1, 1, 1, 1, 1]
+
 evaluate(10)
--- llm_serve.py
+++ llm_serve.py
@@ -4,14 +4,14 @@ from litgpt import LLM
 # export HF_HUB_CACHE="/home/winstonbs/ai/chato_fairy/models"
 from transformers import AutoTokenizer, AutoModelForCausalLM
 def setup():
-    model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-14B-Instruct", torch_dtype="auto", device_map="cuda")
-    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-14B-Instruct") 
+    model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-32B-AWQ", torch_dtype="auto", device_map="cuda")
+    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-32B-AWQ") 
     return (model, tokenizer)
 
 def query_llm(message,llm):
     model, tokenizer = llm
     # Encode the message
     inputs = tokenizer(message, return_tensors="pt").to(model.device)
-    result = model.generate(**inputs, max_new_tokens=500)
+    result = model.generate(**inputs, max_new_tokens=50)
     # Decode the result
     return tokenizer.decode(result[0], skip_special_tokens=True)
